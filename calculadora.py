e528ed9ea2938d105d69ab07d28a78e166b4deb9
Renan Moraes dos Santos <renan.vm96@gmail.com>
2026-05-18 20:58:48 -0300

    feat: calculadora inicial com testes e CI

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+class Calculadora:
+    def soma(self, a, b):
+        return a + b
+
+    def subtracao(self, a, b):
+        return a - b
+
+    def multiplicacao(self, a, b):
+        return a * b
+
+    def divisao(self, a, b):
+        if b == 0:
+            raise ValueError("Divisão por zero não permitida")
+        return a / b
+
+    def potenciacao(self, base, expoente):
+        return base ** expoente
